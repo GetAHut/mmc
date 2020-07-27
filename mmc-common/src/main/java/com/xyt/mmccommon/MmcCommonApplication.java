d4e9942b78fa0f5ee7c724dddd0855ef8403c85d
xuyingtao <10538B7CFD702C93EDE29F24DE0F5899@i-search.com.cn= manager>
2020-07-27 17:33:59 +0800

    1111

---
+++ mmc-common/src/main/java/com/xyt/mmccommon/MmcCommonApplication.java
@@ -0,0 +1,13 @@
+package com.xyt.mmccommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmcCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmcCommonApplication.class, args);
+    }
+
+}
